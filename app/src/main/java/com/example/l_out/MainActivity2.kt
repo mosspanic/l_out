1e305a5b57e113911db2a7ab55895b13c2748ed7
mosspanic <daw_dash@mail.ru>
2021-09-12 11:09:46 +0500

    появилась кнопка

---
+++ app/src/main/java/com/example/l_out/MainActivity2.kt
@@ -0,0 +1,23 @@
+package com.example.l_out
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.l_out.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+
+    }
+
+
+}
